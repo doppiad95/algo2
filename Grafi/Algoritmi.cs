49f78d663af47640016e9934116714e059035a12
Davide Ditolve <d.ditolve@campus.unimib.it>
2018-01-20 01:17:06 +0100

    Aggiunta BFSuntildistance

--- Grafi/Algoritmi.cs
+++ Grafi/Algoritmi.cs
@@ -54,6 +54,55 @@ namespace Grafi
             }
             return visitati;
         }
+        public static List<Nodo> BfsUntilDistance(int start, List<Nodo> daEsaminare,int distanzaDiFine)
+        {
+            List<Nodo> visitati = new List<Nodo>();
+            Nodo primoNodo = daEsaminare.FirstOrDefault(x => x.Id == start);
+            if (primoNodo != null)
+            {
+                primoNodo.Color = Nodo.Colore.Grey.GetHashCode();
+                primoNodo.distanza = 0;
+                primoNodo.Predecessore = null;
+            }
+            List<Nodo> Q = new List<Nodo>
+            {
+                primoNodo
+            };
+            foreach (var nodoCorrente in daEsaminare.Where(x => x.Id != start))
+            {
+                #region initialize
+
+                nodoCorrente.Color = Nodo.Colore.White.GetHashCode();
+                nodoCorrente.distanza = -1;
+                nodoCorrente.Predecessore = null;
+
+                #endregion
+            }
+            while (Q.Count > 0)
+            {
+                Nodo u = Q.FirstOrDefault();
+                foreach (var connessione in u.Connessioni)
+                {
+                    if (connessione.Color == Nodo.Colore.White.GetHashCode())
+                    {
+
+                        connessione.distanza = u.distanza + 1;
+                        connessione.Predecessore = u;
+                        if (connessione.distanza < distanzaDiFine)
+                        {
+                            connessione.Color = Nodo.Colore.Grey.GetHashCode();
+                            Q.Add(connessione);
+                        }
+                        else
+                            connessione.Color = Nodo.Colore.Black.GetHashCode();
+                    }
+                }
+                u.Color = Nodo.Colore.Black.GetHashCode();
+                visitati.Add(u);
+                Q.Remove(u);
+            }
+            return visitati;
+        }
 
         public static List<Nodo> Dfs(List<Nodo> daEsaminare)
         {
